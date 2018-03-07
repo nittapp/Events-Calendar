fa99cf3f8faefa0e419acd3b93c0facdc6e65a92
Sriranganathan Ravi <sriranganathanravi@gmail.com>
2018-03-07 18:26:48 +0530

    Add prefix while fetching headers

--- events/helpers.py
+++ events/helpers.py
@@ -2,12 +2,14 @@ from django.urls import reverse
 from django.shortcuts import render, redirect
 from django.utils.decorators import decorator_from_middleware
 
+header_prefix = "HTTP_" # The prefix used when placing headers into the WSGI environ
+
 def parse_headers(request):
     headers = request.META
     return {
-        'is_admin': headers.get("X_NITT_APP_IS_ADMIN", None) == "true",
-        'name': headers.get("X_NITT_APP_NAME", ""),
-        'user_name': headers.get("X_NITT_APP_USERNAME", "")
+        'is_admin': headers.get(header_prefix + "X_NITT_APP_IS_ADMIN", None) == "true",
+        'name': headers.get(header_prefix + "X_NITT_APP_NAME", ""),
+        'user_name': headers.get(header_prefix + "X_NITT_APP_USERNAME", "")
     }
 
 
